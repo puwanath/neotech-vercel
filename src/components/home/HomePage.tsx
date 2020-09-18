920966d9fe9b29695edaa2457d296dac16ead4ca
9kapong <puwanath.kapongidea@gmail.com>
2020-09-18 20:39:44 +0700

    master13

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,43 +1,16 @@
 // react
 import React, { useMemo } from 'react';
-
 // third-party
 import Head from 'next/head';
-
-// application
-import shopApi from '../../api/shop';
-import { IProduct } from '../../interfaces/product';
-import { useDeferredData, useProductColumns, useProductTabs } from '../../services/hooks';
-
 // blocks
-import BlockBanner from '../blocks/BlockBanner';
 import BlockBrands from '../blocks/BlockBrands';
-import BlockCategories from '../blocks/BlockCategories';
-import BlockFeatures from '../blocks/BlockFeatures';
-import BlockPosts from '../blocks/BlockPosts';
-import BlockProductColumns, { BlockProductColumnsItem } from '../blocks/BlockProductColumns';
-import BlockProducts from '../blocks/BlockProducts';
-import BlockProductsCarousel from '../blocks/BlockProductsCarousel';
 import BlockSlideShow from '../blocks/BlockSlideShow';
 
 // data stubs
-import dataBlogPosts from '../../data/blogPosts';
-import dataShopBlockCategories from '../../data/shopBlockCategories';
 import theme from '../../data/theme';
 
-export interface InitData {
-    featuredProducts?: IProduct[];
-    bestsellers?: IProduct[];
-    latestProducts?: IProduct[];
-    productColumns?: BlockProductColumnsItem[];
-}
 
-export interface HomePageProps {
-    initData?: InitData;
-}
-
-function HomePage(props: HomePageProps) {
-    const { initData } = props;
+function HomePage() {
 
 
     return (
@@ -49,18 +22,6 @@ function HomePage(props: HomePageProps) {
             {useMemo(() => <BlockSlideShow />, [])}
 
             {useMemo(() => <BlockBrands />, [])}
-
-            {/* {useMemo(() => (
-                <BlockCategories
-                    title="Popular Categories"
-                    layout="compact"
-                    categories={dataShopBlockCategories}
-                />
-            ), [])}
-
-
-            {useMemo(() => <BlockPosts title="Latest News" layout="grid-nl" posts={dataBlogPosts} />, [])} */}
-
             
 
         </React.Fragment>
