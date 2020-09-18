c8dd157666aaafec095ebffe1312afcab6677980
9kapong <puwanath.kapongidea@gmail.com>
2020-09-18 20:30:43 +0700

    master11

--- src/interfaces/category.ts
+++ src/interfaces/category.ts
@@ -1,31 +1,12 @@
 import { ICustomFields } from './custom-fields';
 
-// export interface IBaseCategory {
-//     type: string;
-//     id: number;
-//     slug: string;
-//     name: string;
-//     image?: string;
-//     items?: number;
-//     parent?: this;
-//     children?: this[];
-//     customFields: ICustomFields;
-// }
-
-
 export interface IBaseCategory {
-    cat_id: string;
-    cat_id_main: string;
-    cat_name_th: string;
-    cat_name_en: string;
-    cat_detail_th: string;
-    cat_detail_en: string;
-    cat_slug: string;
-    cat_img?: string;
-    cat_noimg?: string;
-    seo_title?: string;
-    seo_desc?: string;
-    seo_keyword?: string;
+    type: string;
+    id: number;
+    slug: string;
+    name: string;
+    image?: string;
+    items?: number;
     parent?: this;
     children?: this[];
     customFields: ICustomFields;
